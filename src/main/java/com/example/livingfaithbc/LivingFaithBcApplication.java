1aca34c7ed4c9c3bb628d10675e7ea7c284ca5a4
Malik <willis.malik.r@gmail.com>
2023-01-24 22:28:11 -0600

    first commit

---
+++ src/main/java/com/example/livingfaithbc/LivingFaithBcApplication.java
@@ -0,0 +1,13 @@
+package com.example.livingfaithbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingFaithBcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivingFaithBcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/livingfaithbc/controllers/HomeController.java
@@ -0,0 +1,2 @@
+package com.example.livingfaithbc.controllers;public class HomeController {
+}
