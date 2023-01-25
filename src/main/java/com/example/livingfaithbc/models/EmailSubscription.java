ac71d6ff2db168ad8842ab8a9a8b0b5738b21f6a
Malik <willis.malik.r@gmail.com>
2023-01-24 22:52:30 -0600

    getters and setters for models

--- src/main/java/com/example/livingfaithbc/models/EmailSubscription.java
+++ src/main/java/com/example/livingfaithbc/models/EmailSubscription.java
@@ -25,6 +25,46 @@ public class EmailSubscription {
     public EmailSubscription() {
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(long phone_number) {
+        this.phone_number = phone_number;
+    }
+
     public EmailSubscription(long id, String firstname, String lastname, String email, long phone_number) {
         this.id = id;
         this.firstname = firstname;
--- src/main/java/com/example/livingfaithbc/models/NewMemberRegistration.java
+++ src/main/java/com/example/livingfaithbc/models/NewMemberRegistration.java
@@ -21,4 +21,54 @@ public class NewMemberRegistration {
     @Column(nullable = false, length = 15)
     private long phone_number;
 
+    public NewMemberRegistration() {
+    }
+
+    public NewMemberRegistration(long id, String firstname, String lastname, String email, long phone_number) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(long phone_number) {
+        this.phone_number = phone_number;
+    }
 }
