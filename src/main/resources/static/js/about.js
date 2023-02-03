7b554fe253cb60e203db20abc79f3f9219576040
Malik <willis.malik.r@gmail.com>
2023-02-02 23:22:42 -0600

    js function blueprint for DOM manipulation for about page complete

--- src/main/resources/static/js/about.js
+++ src/main/resources/static/js/about.js
@@ -1,1 +1,23 @@
-alert("hi");
+(function (){
+    $('#pastorTab').click( function (e){
+        $('.aboutContainer').html(renderAboutPastorInfo)
+    });
+
+    function renderAboutPastorInfo(){
+        let html = '';
+        html += '<div class="aboutBanner">\n' +
+            '        <h1 class="aboutTitle">Pastor Johnson</h1>\n' +
+            '    </div>\n' +
+            '    <div class="aboutImgContainer">\n' +
+            '        <img class="aboutImg" src="../static/images/church_logo.jpg">\n' +
+            '    </div>\n' +
+            '    <div class="aboutInfoRow">\n' +
+            '        <div class="aboutInfoIcon"></div>\n' +
+            '        <div class="aboutInfoTextContainer">\n' +
+            '            <p class="aboutInfoTextBody">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid dolorum earum expedita labore nobis numquam veritatis? Alias, commodi sint!</p>\n' +
+            '            <p class="aboutSignature"></p>\n' +
+            '        </div>\n' +
+            '    </div>';
+        return html;
+    }
+})();
